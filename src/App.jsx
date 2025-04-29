1f3d23796f26af4141175740a508a13da1e2849f
Sharif Hossen <sharifhossen317@gmail.com>
2025-04-29 21:08:42 +0600

    react project set up

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <>
+      <div className="text-white bg-red-600">
+        Hello, welcome to react world!!!
+        <br />
+      </div>
+    </>
+  )
+}
+
+export default App
